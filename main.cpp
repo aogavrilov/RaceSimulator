6495b6eaeb491e5b89ae01a4e3b73fa2d89e13c8
Alexey Gavrilov <alexgavrilov28@gmail.com>
2020-10-15 23:34:13 +0300

    Added basic OOP structure

--- main.cpp
+++ main.cpp
@@ -2,8 +2,10 @@
 #include <vector>
 using namespace std;
 class Vehicle{
+protected:
     float speed;
-protected: Vehicle(float speed) : speed(speed){}
+    Vehicle(float speed) : speed(speed){}
+    float Calculate=0;
 };
 class AirTransport : Vehicle{
     vector<float> delta_percent;
@@ -15,11 +17,37 @@ public:
 class LandTransport : Vehicle{
     float time_to_relax;
     vector<float> relaxing_time;
-    int count_of_relaxstops = 0;
+
 public:
     LandTransport(float speed, float time_to_relax, vector<float> relaxing_time) : Vehicle(speed),
     time_to_relax(time_to_relax), relaxing_time(relaxing_time){}
-
+    float Calculate(float distance){
+        if(distance > speed * time_to_relax){
+            float result_time = 0;
+            float covered_distance = 0;
+            int count_of_relaxstops = 0;
+            while(covered_distance < distance){
+                if(distance - covered_distance <= speed * time_to_relax){
+                    result_time += (distance - covered_distance) / speed;
+                    return result_time;
+                }
+                else{
+                    covered_distance += speed * time_to_relax;
+                    result_time += time_to_relax;
+                    count_of_relaxstops++;
+                    if(count_of_relaxstops < relaxing_time.size()){
+                        result_time += relaxing_time[count_of_relaxstops - 1];
+                    }
+                    else{
+                        result_time += relaxing_time[relaxing_time.size() - 1];
+                    }
+                }
+            }
+        }
+        else{
+            return distance / speed;
+        }
+    }
 
 };
 
