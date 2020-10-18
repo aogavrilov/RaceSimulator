e40d01cb0c9eaa9969c8cb7d0b8302324226d03c
Alexey Gavrilov <alexgavrilov28@gmail.com>
2020-10-18 11:54:51 +0300

    Added basic transport structure

--- main.cpp
+++ main.cpp
@@ -12,7 +12,35 @@ class AirTransport : Vehicle{
     int count_of_relaxstops = 0;
 public:
     AirTransport(float speed, vector<float> percents) : Vehicle(speed), delta_percent(percents){}
-
+    float GetDistancePercent(int count, vector<float> percents){
+        if(count >= percents.size()){
+            return (1 - percents[percents.size() - 1]/100);
+        }
+        else{
+            return (1 - percents[count]/100);
+        }
+    }
+    float Calculate(float distance){
+        if(distance > 1000){
+            float result_time = 0;
+            float covered_distance = 0;
+            int count_of_relaxstops = 0;
+            while(covered_distance < distance){
+                if(distance - covered_distance <= 1000 * GetDistancePercent(count_of_relaxstops, delta_percent)){
+                    result_time += (distance - covered_distance) * GetDistancePercent(count_of_relaxstops, delta_percent) / speed;
+                    return result_time;
+                }
+                else{
+                    covered_distance += 1000;
+                    result_time += (1000 * GetDistancePercent(count_of_relaxstops, delta_percent) / speed);
+                    count_of_relaxstops++;
+                }
+            }
+        }
+        else{
+            return distance / speed;
+        }
+    }
 };
 class LandTransport : Vehicle{
     float time_to_relax;
