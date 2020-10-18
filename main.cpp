9762729a70eac84df02c1aaec72a411cb2fa0452
Alexey Gavrilov <alexgavrilov28@gmail.com>
2020-10-19 02:29:33 +0300

    Added basic race structure and functional

--- main.cpp
+++ main.cpp
@@ -4,14 +4,25 @@ using namespace std;
 class Vehicle{
 protected:
     float speed;
-    Vehicle(float speed) : speed(speed){}
+    size_t type;
+    string name;
+    Vehicle(float speed, string name) : speed(speed), name(name){}
     float Calculate=0;
+public:
+    size_t  GetType(){
+        return type;
+    }
+    string GetName(){
+        return name;
+    }
 };
-class AirTransport : Vehicle{
+class AirTransport : public Vehicle{
     vector<float> delta_percent;
     int count_of_relaxstops = 0;
 public:
-    AirTransport(float speed, vector<float> percents) : Vehicle(speed), delta_percent(percents){}
+    AirTransport(float speed, string name, vector<float> percents) : Vehicle(speed, name), delta_percent(percents){
+        type = 1;
+    }
     float GetDistancePercent(int count, vector<float> percents){
         if(count >= percents.size()){
             return (1 - percents[percents.size() - 1]/100);
@@ -26,7 +37,9 @@ public:
             float covered_distance = 0;
             int count_of_relaxstops = 0;
             while(covered_distance < distance){
-                if(distance - covered_distance <= 1000 * GetDistancePercent(count_of_relaxstops, delta_percent)){
+                //cout << covered_distance << " " << distance << endl;
+                if(distance - covered_distance <= 1000){
+
                     result_time += (distance - covered_distance) * GetDistancePercent(count_of_relaxstops, delta_percent) / speed;
                     return result_time;
                 }
@@ -40,15 +53,18 @@ public:
         else{
             return distance / speed;
         }
+        return 0;
     }
 };
-class LandTransport : Vehicle{
+class LandTransport : public Vehicle{
     float time_to_relax;
     vector<float> relaxing_time;
 
 public:
-    LandTransport(float speed, float time_to_relax, vector<float> relaxing_time) : Vehicle(speed),
-    time_to_relax(time_to_relax), relaxing_time(relaxing_time){}
+    LandTransport(float speed, string name, float time_to_relax, vector<float> relaxing_time) : Vehicle(speed, name),
+    time_to_relax(time_to_relax), relaxing_time(relaxing_time){
+        type = 2;
+    }
     float Calculate(float distance){
         if(distance > speed * time_to_relax){
             float result_time = 0;
@@ -75,12 +91,120 @@ public:
         else{
             return distance / speed;
         }
+        return 0;
     }
 
+
+};
+class MyException : exception{
+private:
+    string m_error;
+public:
+    MyException(string reason) : m_error(reason){}
+    const char* getError() { return m_error.c_str(); }
+};
+class Race{
+protected:
+    Race(){}
+};
+class AirRace : Race{
+    vector<AirTransport> air;
+    size_t type;
+public:
+    AirRace(vector<AirTransport> air, size_t type) : air(air), type(type){}
+
+    AirTransport* Calculate(int dist){
+        int winner_time = air.begin()->Calculate(dist);
+        AirTransport* winner_transport = air.begin().base();
+        for(auto iter = (++air.begin()); iter != air.end(); iter++){
+            float temp = iter->Calculate(dist);
+            if(temp < winner_time){
+                winner_time = temp;
+                winner_transport = iter.base();
+            }
+        }
+        return winner_transport;
+
+    }
+
+};
+class LandRace : Race{
+    vector<LandTransport> landveh;
+    size_t type;
+public:
+    LandRace(vector<LandTransport> landveh, size_t type) : landveh(landveh), type(type){}
+    LandTransport* Calculate(int dist){
+        int winner_time = landveh.begin()->Calculate(dist);
+        LandTransport* winner_transport = landveh.begin().base();
+        for(auto iter = (++landveh.begin()); iter != landveh.end(); iter++){
+            float temp = iter->Calculate(dist);
+            if(temp < winner_time){
+                winner_time = temp;
+                winner_transport = iter.base();
+            }
+        }
+        return winner_transport;
+    }
+};
+
+class MixedRace : Race{
+    vector<LandTransport> landveh;
+    vector<AirTransport> air;
+    size_t type;
+public:
+    MixedRace(vector<LandTransport> landveh, vector<AirTransport> air ) : air(air), landveh(landveh){}
+    Vehicle* Calculate(int dist){
+        if(air.size() == 0)
+            throw MyException("Vector of air vehicle mustn't be empty");
+        if(landveh.size() == 0)
+            throw MyException("Vector of land vehicle mustn't be empty");
+        int winner_air_time = air.begin()->Calculate(dist);
+        AirTransport* winner_air_transport = air.begin().base();
+        for(auto iter = (++air.begin()); iter != air.end(); iter++){
+            float temp = iter->Calculate(dist);
+            if(temp < winner_air_time){
+                winner_air_time = temp;
+                winner_air_transport = iter.base();
+            }
+        }
+
+        int winner_land_time = landveh.begin()->Calculate(dist);
+        LandTransport* winner_land_transport = landveh.begin().base();
+        for(auto iter = (++landveh.begin()); iter != landveh.end(); iter++){
+            float temp = iter->Calculate(dist);
+            if(temp < winner_land_time){
+                winner_land_time = temp;
+                winner_land_transport = iter.base();
+            }
+        }
+        Vehicle* winner_transport;
+        if(winner_air_time < winner_land_time)
+            winner_transport = winner_air_transport;
+        else
+            winner_transport = winner_land_transport;
+        return winner_transport;
+    }
 };
 
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    LandTransport bactrian_camel = LandTransport(10, "bactrian camel", 30, {5, 8});
+    LandTransport speed_camel = LandTransport(40, "speed camel", 10, {5, 6.5, 8});
+    LandTransport centaur = LandTransport(15, "centaur", 8, {2});
+    LandTransport running_boots = LandTransport(6, "running boots", 60, {10, 5});
+
+    AirTransport magic_carpet = AirTransport(10, "magic carpet", {0, 3, 3, 3, 3, 3, 10, 10, 10, 10, 10, 5});
+    AirTransport mortar = AirTransport(8, "mortar", {6});
+
+    AirTransport broom = AirTransport(20, "broom", {1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
+    AirTransport testveh = AirTransport(200, "test vehicle",  {0});
+    AirRace race = AirRace({magic_carpet, mortar, broom, testveh}, 2);
+
+    LandRace race2 = LandRace({bactrian_camel, speed_camel, centaur, running_boots}, 2);
+    MixedRace race3 = MixedRace({bactrian_camel, speed_camel, centaur, running_boots}, {magic_carpet, mortar, broom});
+    cout << race3.Calculate(1000)->GetName();
+    //cout << broom.Calculate(2000);
+    //cout << race2.Calculate(1000)->Calculate(1000);
+
     return 0;
 }
